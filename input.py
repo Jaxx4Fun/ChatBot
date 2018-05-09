4c785e28c64ddbfd7a8dbf7045ed26d3382fe44e
Johnny <309329794@qq.com>
2018-05-09 18:58:28 +0800

    try input now

--- input.py
+++ input.py
@@ -2,27 +2,39 @@ import pyaudio
 from pyaudio import paInt16
 import wave
 import numpy as np
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 # use pyaudio
-RATE = 16000
+RATE = 44100
 FORMAT = paInt16
 CHANNELS = 1
 FRAMES_PER_BUFFER = 1024
+CHUNK = 512
+TIME = 5
 pa = pyaudio.PyAudio()
 stream = pa.open(rate=RATE,
                 format=FORMAT,
                 channels=CHANNELS,
+                frames_per_buffer=FRAMES_PER_BUFFER,
                 input=True)
-for i in range(10):
-    print('time %s \n:'% i)
-    data = stream.read(FRAMES_PER_BUFFER)
-    voice = np.fromstring(data,dtype=np.short)
-    plt.plot(voice)
-    plt.show()
+buffer = []
+for i in range(RATE*TIME//CHUNK):
+    print('CHUNK %s :\n'.ljust(15,'-')% i)
+    try:
+        data = stream.read(CHUNK)
+    except OSError:
+        stream = pa.open(rate=RATE,
+                        format=FORMAT,
+                        channels=CHANNELS,
+                        frames_per_buffer=FRAMES_PER_BUFFER,
+                        input=True)
+        data = stream.read(CHUNK)
 
-    print(voice)
-    print(len(voice))
+    buffer.append(np.fromstring(data,dtype=np.short))
+buff =np.array(buffer).reshape((-1,))
 
+
+# plt.plot(buff)
+# plt.show()
 # # use arecord
 # import os
 # os.system('arecord -d 10 ')
