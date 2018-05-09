8f8bea2dbf1ef0c37a2524b8568e1a027f18ddd7
Johnny <309329794@qq.com>
2018-05-09 15:43:52 +0800

    1

--- input.py
+++ input.py
@@ -1,6 +1,9 @@
 import pyaudio
 from pyaudio import paInt16
 import wave
+import numpy as np
+from matplotlib import pyplot as plt
+# use pyaudio
 RATE = 16000
 FORMAT = paInt16
 CHANNELS = 1
@@ -10,6 +13,16 @@ stream = pa.open(rate=RATE,
                 format=FORMAT,
                 channels=CHANNELS,
                 input=True)
-while True:
+for i in range(10):
+    print('time %s \n:'% i)
     data = stream.read(FRAMES_PER_BUFFER)
-    print(data)
+    voice = np.fromstring(data,dtype=np.short)
+    plt.plot(voice)
+    plt.show()
+
+    print(voice)
+    print(len(voice))
+
+# # use arecord
+# import os
+# os.system('arecord -d 10 ')
--- test_pyaudio.py
+++ test_pyaudio.py
@@ -6,7 +6,7 @@ from aip import AipSpeech
 class ChatBot:
     def __init__(self, *args, **kwargs):
         self.CLIENT =AipSpeech(
-            appID = '10528707',
+            appId = '10528707',
             apiKey = 'iBCuQlT14vkiPzr2qzEYpu6s',
             secretKey = '7znifePL2YhGeKgqSsw2Q33sXqsIG6x2'
         )
@@ -19,15 +19,14 @@ class ChatBot:
         # 输入 - 输出
         # response = self.DIALOGUE.get_response(text)
 
-
-    def recognize_audio(self,audio_data):
-        buffer = 0
+    def record_audio(self,time=5):
+        os.system ('arecord -D "plughw:1,0" -d 10 {dir}'.format(dir=self.AUDIO_DIR))
         with open(self.AUDIO_DIR) as audio:
             buffer = audio.read()
+            return buffer
+    def recognize_audio(self,buffer):
         self.CLIENT.asr(speech=buffer,format='wav',rate=16000)
 
-    def record_audio(self,time=5):
-        os.system ('arecord -D "plughw:1,0" -d 10 > {dir}'.format(dir='./output.wav'))
 
     def compose_audio(self,text_data='我是一头猪呀'):
         result = self.CLIENT.synthesis(text_data)
@@ -40,50 +39,48 @@ class ChatBot:
             os.system('aplay %s'%self.RESPONSE_DIR)
 
 
-def record_audio():
+    def record_audio_by_pa(self):
 
-    CHUNK = 512
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = "output.wav"
+        CHUNK = 512
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 44100
+        RECORD_SECONDS = 5
+        WAVE_OUTPUT_FILENAME = "output.wav"
 
-    p = pyaudio.PyAudio()
+        p = pyaudio.PyAudio()
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
 
-    print("recording...")
+        print("recording...")
 
-    frames = []
+        frames = []
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
 
-    print("done")
+        print("done")
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
 
 
 if __name__ == '__main__':
     cb = ChatBot()
     # test compose
-    cb.compose_audio()
-    # test recognize
-    cb.record_audio
+    cb.run()
 
 
